1d924d6f21c9179e9fe7abbe81882b8458309274
nursh <nsheikhmohammed01@gmail.com>
2021-08-20 03:27:53 -0400

    Setup apollo graphql for server needs

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Application } from "express";
+import { ApolloServer } from "apollo-server-express";
+
+import { typeDefs, resolvers } from './graphql';
+// import { connectDatabase } from "./db/mongodb";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const port = process.env.PORT || 8080;
+
+async function start(app: Application) {
+  // const db = await connectDatabase();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req, res }) => ({
+      // db,
+      req,
+      res,
+    }),
+  });
+
+  await server.start();
+  server.applyMiddleware({ app, path: "/api" });
+  app.listen(port, () => console.log(`[app]: running on port: ${port}${server.graphqlPath}`));
+}
+
+start(app);

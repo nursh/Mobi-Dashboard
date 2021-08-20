9336ace17f5607da7c6c546c1a3590775efbcf88
nursh <nsheikhmohammed01@gmail.com>
2021-08-20 16:15:01 -0400

    Refactore some code in graphql and database

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 import { Database, User, Campaign } from './types';
 
 const url =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.abx3p.mongodb.net/dev?retryWrites=true&w=majority`;
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/dev?retryWrites=true&w=majority`;
 
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url);
